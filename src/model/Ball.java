2113b651aae54412789d22ff0af40da19b30d568
oliver steptoe <oliver.steppers@gmail.com>
2017-02-10 21:48:52 +0000

    Ball inflated

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -5,6 +5,8 @@ package model;
  */
 public class Ball {
 
+    private static final float ACCEL = 0.7f;
+
     private float xf, yf;
     private int xi, yi;
 
@@ -29,13 +31,70 @@ public class Ball {
 
     public void update(float delta) {
         if(moving) {
+            switch(dir) {
+                case UP:
+                    velY += ACCEL * delta;
+                    yf += velY * delta;
+                    if(yf > yTarget) {
+                        moving = false;
+                        yf = yTarget;
+                        yi = yTarget;
+                    }
+                    break;
 
+                case DOWN:
+                    velY -= ACCEL * delta;
+                    yf += velY * delta;
+                    if(yf < yTarget) {
+                        moving = false;
+                        yf = yTarget;
+                        yi = yTarget;
+                    }
+                    break;
+
+                case RIGHT:
+                    velX += ACCEL * delta;
+                    xf += velX * delta;
+                    if(xf > xTarget) {
+                        moving = false;
+                        xf = xTarget;
+                        xi = xTarget;
+                    }
+                    break;
+
+                case LEFT:
+                    velX -= ACCEL * delta;
+                    xf += velX * delta;
+                    if(xf < xTarget) {
+                        moving = false;
+                        xf = xTarget;
+                        xi = xTarget;
+                    }
+                    break;
+            }
         }
     }
 
     public void setTarget(int x, int y) {
         moving = true;
+        int dx = x - xi;
+        int dy = y - yi;
 
+        if(dx == 0) {
+            if(dy > 0) {
+                dir = Direction.UP;
+            } else {
+                dir = Direction.DOWN;
+            }
+        } else {
+            if(dx > 0) {
+                dir = Direction.RIGHT;
+            } else {
+                dir = Direction.LEFT;
+            }
+        }
+        velX = 0;
+        velY = 0;
     }
 
 }
