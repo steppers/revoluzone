9a68ef24fb66196723491d96fed10c812f188585
antonnikitin97 <antonnikitin@live.co.uk>
2017-02-11 22:24:47 +0000

    added more tools to editor - changed keybindings to numbers

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,7 +16,7 @@ public class Main {
         AppGameContainer gc;
         try{
             gc = new AppGameContainer(new StateManager(GAME_NAME));
-            gc.setDisplayMode(2400, 1350, false);
+            gc.setDisplayMode(gc.getScreenWidth(), gc.getScreenHeight(), false);
             gc.setVSync(true);
             gc.setSmoothDeltas(true);
             gc.setMultiSample(4);
