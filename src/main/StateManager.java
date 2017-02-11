83c7bc7dfaf7f64f6ab2d01ca4b4d1d78899c550
Anton Nikitin <antonnikitin@live.co.uk>
2017-02-11 11:37:43 +0000

    removed menu and highscore states

--- src/main/StateManager.java
+++ src/main/StateManager.java
@@ -1,11 +1,8 @@
 package main;
 
-import graphics.FontLoader;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
-import states.HighScoreScreen;
-import states.Menu;
 import states.World;
 
 public class StateManager extends StateBasedGame {
@@ -21,9 +18,7 @@ public class StateManager extends StateBasedGame {
 
     @Override
     public void initStatesList(GameContainer gameContainer) throws SlickException {
-        this.addState(new Menu(StateId.MENU.ordinal()));
         this.addState(new World(StateId.WORLD.ordinal()));
-        this.addState(HighScoreScreen.getInstance(StateId.HIGHSCORE.ordinal()));
         this.enterState(StateId.HIGHSCORE.ordinal());
     }
 }
