f7c6e73e16caad9f23d5e6297a50167da03fb3d8
oliver steptoe <oliver.steppers@gmail.com>
2017-02-12 13:35:59 +0000

    Add more render capability to tile

--- src/proto/Tile.java
+++ src/proto/Tile.java
@@ -2,6 +2,7 @@ package proto;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.geom.Transform;
@@ -84,10 +85,19 @@ public class Tile {
         }
     }
 
-
     public void render(int x, int y, int size, Graphics g, float opacity) {
         Rectangle rect = new Rectangle(x, y, size, size);
         Shape stripe = rect.transform(Transform.createScaleTransform(0.333f, 1f));
+        stripe = stripe.transform(Transform.createTranslateTransform(+size/2, 0));
+
+        //Switch rect
+        Rectangle swrect = new Rectangle(x+(size*0.2f), y+(size*0.2f), size*0.6f, size*0.6f);
+        Circle cicleSwitch = new Circle(x+size/2,y+size/2, size*0.5f*0.4f);
+
+        //Start/finish
+        Circle circleLarge = new Circle(x+size/2,y+size/2, size*0.5f*0.8f);
+        Circle circleSmall = new Circle(x+size/2,y+size/2, size*0.5f*0.7f);
+
         switch(type) {
             case EMPTY:
                 g.setColor(Color.white.darker(0.2f).multiply(new Color(1,1,1,opacity)));
@@ -112,16 +122,35 @@ public class Tile {
                 g.fill(stripe);
                 break;
             case START:
+                g.setColor(Color.green.darker().multiply(new Color(1,1,1,opacity)));
+                g.fill(circleLarge);
+                g.setColor(Color.green.multiply(new Color(1,1,1,opacity)));
+                g.fill(circleSmall);
                 break;
             case FINISH:
+                g.setColor(Color.darkGray.multiply(new Color(1,1,1,opacity)));
+                g.fill(circleLarge);
+                g.setColor(Color.black.multiply(new Color(1,1,1,opacity)));
+                g.fill(circleSmall);
                 break;
             case SWITCH:
+                g.setColor(Color.darkGray.multiply(new Color(1,1,1,opacity)));
+                g.fill(swrect);
+                if(active)
+                    g.setColor(Color.green.multiply(new Color(1,1,1,opacity)));
+                else
+                    g.setColor(Color.red.multiply(new Color(1,1,1,opacity)));
+                g.fill(cicleSwitch);
                 break;
             case SLIDE:
                 break;
             case TELEPORT:
                 break;
             case LOCKED_FINISH:
+                g.setColor(Color.darkGray.multiply(new Color(1,1,1,opacity)));
+                g.fill(circleLarge);
+                g.setColor(Color.red.multiply(new Color(1,1,1,opacity)));
+                g.fill(circleSmall);
                 break;
             case BLUE_FINISH:
                 break;
