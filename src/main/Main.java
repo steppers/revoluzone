fd282320e8f28778cfd01067dae9c089bb57b3db
Oliver Steptoe <oliver.steppers@gmail.com>
2017-05-20 01:36:23 +0100

    Default fullscreen 1080p HD no FPS count

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,11 +17,12 @@ public class Main {
         try{
             gc = new AppGameContainer(new StateManager(GAME_NAME));
 //            gc.setDisplayMode(gc.getScreenWidth()-100, gc.getScreenHeight()-80, false);
-//            gc.setDisplayMode(1920, 1080, true);
-            gc.setDisplayMode(1280, 720, false);
+            gc.setDisplayMode(1920, 1080, true);
+//            gc.setDisplayMode(1280, 720, false);
             gc.setVSync(true);
             gc.setSmoothDeltas(true);
             gc.setMultiSample(4);
+            gc.setShowFPS(false);
             gc.start();
         }catch (Exception e){
             System.out.println(e.getMessage());
