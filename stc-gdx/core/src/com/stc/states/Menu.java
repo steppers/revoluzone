3f65a5247998e62015cd9a0e20b71ac88a823751
steppers <oliver.steppers@gmail.com>
2017-07-13 20:49:47 +0100

    Exit on touch/mouse click (For testing)

--- stc-gdx/core/src/com/stc/states/Menu.java
+++ stc-gdx/core/src/com/stc/states/Menu.java
@@ -1,5 +1,6 @@
 package com.stc.states;
 
+import com.badlogic.gdx.Gdx;
 import com.stc.Main;
 import com.stc.State;
 
@@ -13,6 +14,11 @@ public class Menu implements State {
     public void update(float delta) {
         Main.model.rotation += 120 * delta;
         Main.model.update(delta);
+
+        if(Gdx.input.isTouched()) {
+            System.out.println("Screen touched. Exiting...");
+            Gdx.app.exit();
+        }
     }
 
     @Override
