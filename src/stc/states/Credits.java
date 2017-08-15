287afec5ed932df818b4ee689448c116eddb7eed
Ali <escrime.ali@virginmedia.com>
2017-08-15 12:30:39 +0100

    2 more levels, added dan to the credits, made background more fancy

--- src/stc/states/Credits.java
+++ src/stc/states/Credits.java
@@ -1,5 +1,6 @@
 package stc.states;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
@@ -21,42 +22,55 @@ public class Credits {
     private Model m;
 
     private ArrayList<UIRenderable> rotatingUI;
+    private ArrayList<UIRenderable> staticUI;
+
+    private String[] names = {"Oliver Steptoe", "Alistair Brewin", "Anton Nikitin", "Daniel Bradley", "Many thanks to Southampton Game Jam 2017"};
 
     public Credits(GameState gameState, TransitionManager tm, GameContainer gc) {
         gs = gameState;
         this.tm = tm;
         this.m = gs.m;
 
+        //Static UI
+        staticUI = new ArrayList<>();
+        UILabel tmpLabel = new UILabel(gc);
+        tmpLabel.text = names[4];
+        tmpLabel.anchor.set(0.5f, 0.5f);
+        tmpLabel.offset.set(0f, 0.7f);
+        tmpLabel.color = Color.green.darker(0.4f);
+        staticUI.add(tmpLabel.clone());
+
+
         //Rotating UI
         rotatingUI = new ArrayList<>();
-        UILabel tmpLabel = new UILabel("Oliver Steptoe", gc);
+        tmpLabel.color = Color.white;
+        tmpLabel.text = names[0];
         tmpLabel.anchor.set(0.5f, 0.5f);
         tmpLabel.offset.set(0f, -0.46f);
         tmpLabel.scale = m.getScale()/0.6f;
         rotatingUI.add(tmpLabel.clone());
-        tmpLabel.text = "Alistair Brewin";
+        tmpLabel.text = names[1];
         tmpLabel.rotation = -90f;
         rotatingUI.add(tmpLabel.clone());
-        tmpLabel.text = "Anton Nikitin";
+        tmpLabel.text = names[2];
         tmpLabel.rotation = 180f;
         rotatingUI.add(tmpLabel.clone());
-        tmpLabel.text = "Many thanks to\n" +
-                "Southampton Game Jam\n" +
-                "2017";
+        tmpLabel.text = names[3];
         tmpLabel.rotation = 90f;
         tmpLabel.offset.set(0f, -0.5f);
         rotatingUI.add(tmpLabel.clone());
     }
 
-    public void update(GameContainer gc, float delta) {
+    public void update(GameContainer gc) {
         m = gs.m;
-        m.setRotation(m.getRotation() + 35f * delta);
         if(gc.getInput().isKeyDown(Input.KEY_ESCAPE)) {
             tm.transitionFade(m, new Model(m.getProperty("filename"), 0.6f, 0.3f), GameState.State.MENU, 0.4f);
         }
-
-        for(UIRenderable r : rotatingUI) {
-            r.update();
+        else if(gc.getInput().isKeyPressed(Input.KEY_RIGHT)) {
+            tm.transitionRotate(m, gs.currentState, 90, 0.2f);
+        }
+        else if(gc.getInput().isKeyPressed(Input.KEY_LEFT)) {
+            tm.transitionRotate(m, gs.currentState, -90, 0.2f);
         }
     }
 
@@ -70,6 +84,12 @@ public class Credits {
             r.color.a = m.getOpacity();
             r.scale = m.getScale()/0.6f;
             r.offsetRotation(m.getRotation());
+            r.scaleOffset(m.getScale());
+            r.render(g);
+        }
+        for(UIRenderable r : staticUI) {
+            r.color.a = m.getOpacity();
+            r.scale = m.getScale()/0.6f;
             r.scaleOffset(m.getScale());
             r.render(g);
         }
